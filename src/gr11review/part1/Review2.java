ab5463388e30b8588fe2cb566ec8c52fa03b553e
alyannasantos <>
2021-05-02 22:16:36 +0000

    Edit Review2

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -19,23 +19,23 @@ public class Review2 {
 
     // Joke contents
     switch (intOption) {
-      case 0: 
+      case 0 : 
         System.out.println("Why do bees have sticky hair?");
         System.out.println("They always use honeycombs.");
         break;
-      case 1: 
+      case 1 : 
         System.out.println("Did you hear about the four foot tall psychic who escaped prison?");
         System.out.println("He's a small medium at large.");
         break;
-      case 2: 
+      case 2 : 
         System.out.println("What’s a baby’s favorite clothing brand?");
         System.out.println("Fruit of the womb.");
         break;
-      case 3: 
+      case 3 : 
         System.out.println("Why were the teacher's eyes crossed?");
         System.out.println("Because she couldn't control her pupils.");
         break;
-      default:
+      default :
         System.out.println("Invalid menu option");
         break;
     } 
