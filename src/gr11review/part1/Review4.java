0f8d03bbefacd26561e1d61074d6f16eab1f90d5
alyannasantos <>
2021-05-02 21:54:14 +0000

    Review5

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,1 +1,34 @@
 package gr11review.part1;
+
+import java.text.DecimalFormat;
+import java.io.*;
+
+public class Review4 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+    DecimalFormat decimalFormat = new DecimalFormat("$###,####,##0.00");
+    double dblSubtotal = 0.0;
+    double dblTax = 0.0;
+    double dblTotal = 0.0;
+
+    System.out.print("How many items do you want to buy? ");
+    int intItemCount = Integer.parseInt(key.readLine());
+    
+    for (int intCount = 1; intCount <= intItemCount; intCount++) {
+      System.out.print("Enter the price for item " + intCount + ": ");
+      double dblAmount = Double.parseDouble(key.readLine());
+      dblSubtotal = dblSubtotal + dblAmount;
+    }
+
+    dblTax = dblSubtotal * 0.13;
+    dblTotal = dblSubtotal + dblTax;
+
+    System.out.println("Subtotal: " + decimalFormat.format(dblSubtotal));
+    System.out.println("Tax: " + decimalFormat.format(dblTax));
+    System.out.println("Total: " + decimalFormat.format(dblTotal));
+
+  }
+
+}
--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,1 +1,47 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review5 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+    // Initialize variables
+    double dblInvestedAmount = 0.0;
+    double dblInterestRate = 0.0;
+    double dblTargetAmount = 0.0;
+    int intTotalYears = 0;
+    double dblAccumulatedTotal = 0.0;
+
+    // Get user inputs
+    System.out.print("Enter the yearly invested amount: ");
+    dblInvestedAmount = Double.parseDouble(key.readLine());
+
+    System.out.print("Enter the compound interest rate: ");
+    dblInterestRate = Double.parseDouble(key.readLine());
+
+    System.out.print("Enter the target amount: ");
+    dblTargetAmount = Double.parseDouble(key.readLine());
+
+    double dblConvertedInterestRate = dblInterestRate / 100;
+    double dblEarnedAmount = 0.0;
+
+    // Calculate total years
+    while (dblAccumulatedTotal < dblTargetAmount) {
+      dblAccumulatedTotal = dblAccumulatedTotal + dblInvestedAmount;
+      dblEarnedAmount = dblConvertedInterestRate * dblAccumulatedTotal;
+      dblAccumulatedTotal = dblAccumulatedTotal + dblEarnedAmount; 
+
+      // Increment years
+      intTotalYears++;
+    }
+
+
+    System.out.println("The target amount will be earned in " + intTotalYears + " years.");
+
+
+
+  }
+
+}
