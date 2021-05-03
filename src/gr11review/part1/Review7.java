eec0da603511bc7215c020f878fc038bea1a4bb1
alyannasantos <>
2021-05-03 00:26:45 +0000

    Complete Review7

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -1,1 +1,46 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review7 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+    // Initialize variables
+    int intSpace = 0;
+    int intLetterA = 0;
+    String strOddLetters = "";
+
+    // User input
+    String theSentence = key.readLine();
+
+    // Number of characters
+    int intCharacters = theSentence.length();
+    System.out.println("There are " + intCharacters + " characters in the sentence.");
+
+    // Calculate number of spaces, letter a, and odd number
+    for (int intI = 0; intI < intCharacters; intI++) {
+      char charLetter = theSentence.charAt(intI);
+      
+      if (charLetter == ' ') {
+        intSpace++;
+      }
+
+      if (charLetter == 'a') {
+        intLetterA++;
+      }
+
+      if ((intI % 2) == 0) {
+        strOddLetters = strOddLetters + "-";
+      }
+    }
+
+    // Print out
+    System.out.println("There are " + intSpace + " spaces in the sentence.");
+    System.out.println("There are " + intLetterA + " letter a in the sentence.");
+    System.out.println(strOddLetters);
+
+  }
+
+}
