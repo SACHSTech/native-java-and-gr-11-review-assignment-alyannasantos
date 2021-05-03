5370ade37773369bb71ce156f516cb587e4b6b3b
alyannasantos <>
2021-05-03 01:25:05 +0000

    Comments on Review2

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -7,12 +7,13 @@ public class Review2 {
   public static void main(String[] args) throws IOException {
     BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
-    // Options
+    // Joke option outputs
     System.out.println("0 - print a joke about your hair");
     System.out.println("1 - print a joke about your feet");
     System.out.println("2 - print a joke about your clothes");
     System.out.println("3 - print a joke about your teacher");
 
+    // User input 
     System.out.print("Choose a menu option: ");
     int intOption = Integer.parseInt(key.readLine());
 
